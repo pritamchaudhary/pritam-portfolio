94e8cafb98fe01bbf4ad640edba52c25d7ccdec0
Pritam kumar <92274739+pritamchaudhary@users.noreply.github.com>
2025-09-01 17:53:51 +0530

    add resume download

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -20,7 +20,7 @@ const Header = () => {
         <div className='flex flex-col sm:flex-row items-center gap-4 mt-4'>
             <motion.a initial={{y: 30, opacity: 0}} whileInView={{y: 0, opacity: 1}} transition={{duration: 0.6, delay: 1}} href="#contact" className='px-10 py-3 border border-white rounded-full bg-black text-white flex items-center gap-2 dark:bg-transparent'>contact me <Image src={assets.right_arrow_white} alt='' className='w-4' /></motion.a>
 
-            <motion.a initial={{y: 30, opacity: 0}} whileInView={{y: 0, opacity: 1}} transition={{duration: 0.6, delay: 1.2}} href="/pritam-resume.pdf" download className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 bg-white dark:text-black'>My resume <Image src={assets.download_icon} alt='' className='w-4' /></motion.a>
+            <motion.a initial={{y: 30, opacity: 0}} whileInView={{y: 0, opacity: 1}} transition={{duration: 0.6, delay: 1.2}} href="/pritam_resume.pdf" download className='px-10 py-3 border rounded-full border-gray-500 flex items-center gap-2 bg-white dark:text-black'>My resume <Image src={assets.download_icon} alt='' className='w-4' /></motion.a>
         </div>
     </div>
   )
